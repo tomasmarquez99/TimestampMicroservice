30a0ecdd5e699731c0f778a2c50567b54c856fef
Tomás Marquez <btomasmarquez@gmail.com>
2024-04-28 00:38:37 +0000

    challenge almost resolved

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,43 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+
+
+/*const responseApi = (unix, timestamp) => {
+  unix: unix,
+  timestamp: timestamp,
+  toUnix(){
+    return Math.floor(timestamp/1000)}
+}
+*/
+
+app.get("/api/:date?", (req, res) => {
+  const { date } = req.params;
+  
+  const dateString = { date }.date
+  console.log("datestring: "+ dateString)
+  
+  //utc string time working
+  const utcTime = new Date(Date.parse(dateString));
+  console.log("utcTime:" + utcTime)
+
+  const unixTime = Date.parse(utcTime)
+  
+    if (dateString) {
+    res.json({ unix: unixTime, utc: utcTime })
+  } else {
+    //working - applies when there isn't parameter text for /api/...
+    const unixDate = Date.now()
+    const utcDate = new Date(unixDate)
+    const dateString = utcDate.toUTCString();
+    res.json({ unix: unixDate, utc: dateString })
+  }
+
+
+})
 
 
 
